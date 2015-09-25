fc2b8128c789b064bd3b67e8b9bafacdbc48d691
Giles Lloyd <giles@propcom.co.uk>
2015-09-25 14:45:51 +0100

    Add form js

---
+++ js/form-page.js
@@ -0,0 +1,21 @@
+$(function(){
+	$(document).on('submit', '.js-form-page', function(event){
+		event.preventDefault();
+		var form = $(this);
+		var data = form.serialize();
+		var url = form.data('url');
+
+		$.ajax({
+			url: url+'.json',
+			data: data,
+			dataType: 'JSON',
+			type: 'POST',
+			success: function(data){
+				form.replaceWith($("<span>"+ data.message +"</span>"));
+			},
+			error: function(data){
+				$(".js-form-page-error").text(data.responseText);
+			}
+		});
+	});
+});
